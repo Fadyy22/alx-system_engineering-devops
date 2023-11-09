dc3c598a92d11a56383bb88f38fa477c199314a4
Fadyy22 <fadyalaa441@gmail.com>
2023-11-09 02:51:38 +0200

    puppet script that fixes apache2 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Script that fixes 500 apache error
-
-exec { 'fix_apache2':
+exec {'fix_apache2':
     provider => shell,
-    exec     => sed -i 's/phpp/php/g' /var/www/html/wp-settings.php
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
